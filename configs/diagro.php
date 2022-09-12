081ed3f09b6e8dbda992e853bdd4f940a24be323
Stijn Leenknegt <stijn@diagro.be>
2022-09-12 09:13:02 +0200

    version 2.9
    JWKS usage in tokens

--- configs/diagro.php
+++ configs/diagro.php
@@ -1,12 +1,14 @@
 <?php
 return [
 
-    'app_id' => env('DIAGRO_APP_ID', null),
+    'app_id' => env('DIAGRO_APP_ID'),
 
-    'app_name' => env('DIAGRO_APP_NAME', null),
+    'app_name' => env('DIAGRO_APP_NAME'),
 
-    'service_auth_uri' => env('DIAGRO_SERVICE_AUTH_URI', null),
+    'service_auth_uri' => env('DIAGRO_SERVICE_AUTH_URI'),
 
-    'service_metric_uri' => env('DIAGRO_SERVICE_METRIC_URI', null),
+    'service_metric_uri' => env('DIAGRO_SERVICE_METRIC_URI'),
+
+    'service_jwks_uri' => env('DIAGRO_SERVICE_JWKS_URI'),
 
 ];
